e73eee2f84f3e1ecfd0eac2caa3e2edc17596897
Eddyfabbricatore <eddyfabbricatore2@gmail.com>
2023-10-04 14:06:07 +0200

    js [add] searchFor [add] deleteSearch

--- script.js
+++ script.js
@@ -8,6 +8,7 @@ createApp({
             name: 'Michele',
             avatar: './img/avatar_1.jpg',
             visible: true,
+            notSearch: false,
             messages: [
                 {
                     date: '10/01/2020 15:30:55',
@@ -30,19 +31,20 @@ createApp({
             name: 'Fabio',
             avatar: './img/avatar_2.jpg',
             visible: false,
+            notSearch: false,
             messages: [
                 {
-                    date: '20/03/2020 16:30:00',
+                    date: '03/20/2020 16:30:00',
                     message: 'Ciao come stai?',
                     status: 'sent'
                 },
                 {
-                    date: '20/03/2020 16:30:55',
+                    date: '03/20/2020 16:30:55',
                     message: 'Bene grazie! Stasera ci vediamo?',
                     status: 'received'
                 },
                 {
-                    date: '20/03/2020 16:35:00',
+                    date: '03/20/2020 16:35:00',
                     message: 'Mi piacerebbe ma devo andare a fare la spesa.',
                     status: 'sent'
                 }
@@ -52,19 +54,20 @@ createApp({
             name: 'Samuele',
             avatar: './img/avatar_3.jpg',
             visible: false,
+            notSearch: false,
             messages: [
                 {
-                    date: '28/03/2020 10:10:40',
+                    date: '03/28/2020 10:10:40',
                     message: 'La Marianna va in campagna',
                     status: 'received'
                 },
                 {
-                    date: '28/03/2020 10:20:10',
+                    date: '03/28/2020 10:20:10',
                     message: 'Sicuro di non aver sbagliato chat?',
                     status: 'sent'
                 },
                 {
-                    date: '28/03/2020 16:15:22',
+                    date: '03/28/2020 16:15:22',
                     message: 'Ah scusa!',
                     status: 'received'
                 }
@@ -74,6 +77,7 @@ createApp({
             name: 'Alessandro B.',
             avatar: './img/avatar_4.jpg',
             visible: false,
+            notSearch: false,
             messages: [
                 {
                     date: '10/01/2020 15:30:55',
@@ -91,6 +95,7 @@ createApp({
             name: 'Alessandro L.',
             avatar: './img/avatar_5.jpg',
             visible: false,
+            notSearch: false,
             messages: [
                 {
                     date: '10/01/2020 15:30:55',
@@ -108,6 +113,7 @@ createApp({
             name: 'Claudia',
             avatar: './img/avatar_6.jpg',
             visible: false,
+            notSearch: false,
             messages: [
                 {
                     date: '10/01/2020 15:30:55',
@@ -130,6 +136,7 @@ createApp({
             name: 'Federico',
             avatar: './img/avatar_7.jpg',
             visible: false,
+            notSearch: false,
             messages: [
                 {
                     date: '10/01/2020 15:30:55',
@@ -147,6 +154,7 @@ createApp({
             name: 'Davide',
             avatar: './img/avatar_8.jpg',
             visible: false,
+            notSearch: false,
             messages: [
                 {
                     date: '10/01/2020 15:30:55',
@@ -167,23 +175,41 @@ createApp({
         }
       ],
       counter: 0,
-      newMessage: ''
+      newMessage: '',
+      search: ''
     }
   },
   methods:{
     changeChat(i){
-      console.log(i);
       this.contacts[this.counter].visible = false
 
       this.counter = i;
 
-      console.log(i);
-
       this.contacts[this.counter].visible = true;
     },
 
+    searchFor(){
+      const searchString = this.search.toLowerCase();
+
+      this.contacts.filter((n, index) => {
+        if(!(n.name.toLowerCase().includes(searchString))){
+          this.contacts[index].notSearch = true;
+        }
+      })
+    },
+
+    deleteSearch(){
+      this.search.slice(0, - 1);
+
+      this.contacts.forEach((contact) => {
+        contact.notSearch = false;
+      })
+
+      this.searchFor();
+    },
+
     getHour(dateMessage){
-      const d = new Date(dateMessage);
+      const d = new Date((dateMessage));
       let hour = d.getHours();
       let minute = d.getMinutes();
 
@@ -191,15 +217,17 @@ createApp({
     },
 
     createNewMessage(i){
-      const createMessage = {
-        date: new Date(),
-        message: this.newMessage,
-        status: 'sent'
-      }
-
-      this.contacts[i].messages.push(createMessage);
-
-      this.newMessage = '';
+      setTimeout(() => {
+        const createMessage = {
+          date: new Date(),
+          message: this.newMessage,
+          status: 'sent'
+        }
+  
+        this.contacts[i].messages.push(createMessage);
+  
+        this.newMessage = '';
+      }, 500)
 
       setTimeout(() => {
         const createMessageReceived = {
@@ -209,7 +237,7 @@ createApp({
         }
   
         this.contacts[i].messages.push(createMessageReceived);
-      }, 1000)
+      }, 1500)
     }
   }
 }).mount('#app')
